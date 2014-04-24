405eec43d35c2002d14a6e0b21275efaec0c8b47
dennisyu <dennisyu@mini147.(none)>
2014-04-24 17:21:15 +0800

    Add the MinimizeLP

---
+++ Reasearch/LP_MLS.m
@@ -0,0 +1,73 @@
+correlation3 =load('test3LpInput.txt');
+Capacity = [1.86313e+06 1.86313e+06 2.06955e+06 1.86313e+06 1.86313e+06 ...
+    2.06955e+06 113231 113231 110986 113231 113231 110986 3.6668e+06  ...
+    3.6668e+06 3.02487e+06 3.6668e+06 3.6668e+06 3.02487e+06 3.6668e+06 ...
+    3.6668e+06 3.02487e+06 3.6668e+06 3.6668e+06 3.02487e+06];
+cameraNum = length(correlation3(:,1));
+
+%---Objective---%
+f = zeros(cameraNum*(cameraNum+1)+1,1);
+f(cameraNum*(cameraNum+1)+1,1)=1;
+
+%---Bounds---%
+lb=zeros(cameraNum*(cameraNum+1)+1,1);
+lb(cameraNum*(cameraNum+1)+1,1) = -inf;
+ub=ones(cameraNum*(cameraNum+1)+1,1);
+ub(cameraNum*(cameraNum+1)+1,1) = inf;
+
+%---Inequalities---%
+A = zeros(cameraNum*(cameraNum+1)+cameraNum,cameraNum*(cameraNum+1)+1);
+b = zeros(cameraNum*(cameraNum+1)+cameraNum,1);
+%Each camera has at least one indep camera to overhear
+for k=1:cameraNum
+    A(k,find(correlation3(k,:)>0))= -1;
+    b(k) = -1;
+end
+%v_j must indep. sent before v_i can overhear v_j
+for j=1:cameraNum
+    for i=1:cameraNum
+        A(j*cameraNum+i,j*cameraNum+i) = 1;
+        A(j*cameraNum+i,j) = -1;
+    end
+end
+%Transmisssion time constraint y_{qp}*H(X_p|X_q)/C_{p0} - z < 0
+for p=1:cameraNum
+    temp_p = [];
+    temp_p = find(correlation3(p,:)>0);
+    for n=1:length(temp_p)
+        q=temp_p(n);
+        A(cameraNum*(cameraNum+1)+p,q*cameraNum+p) = correlation3(p,q)/Capacity(p);
+    end
+    A(cameraNum*(cameraNum+1)+p,cameraNum*(cameraNum+1)+1) = -1; % -z
+end
+
+%---Equalities---%
+Aeq = zeros(2*cameraNum,cameraNum*(cameraNum+1)+1);
+beq = zeros(2*cameraNum,1);
+%Each camera overhear only once
+for ii=1:cameraNum
+    temp = [];
+    temp = find(correlation3(ii,:)>0);
+    for kk=1:length(temp)
+        jj=temp(kk);
+        Aeq(ii,jj*cameraNum+ii) = 1;
+    end
+    beq(ii)=1;
+end
+%send indep. = overhear itself
+for m=1:cameraNum
+    Aeq(cameraNum+m,m*cameraNum+m) = 1;
+    Aeq(cameraNum+m,m) = -1;
+end
+
+% optx(1:24) is x optimal
+% optx(25:48) is y_{1,1:24} optimal ...etc
+[optx fval] = linprog(f,A,b,Aeq,beq,lb,ub);
+X = optx(1:cameraNum);
+for r=1:cameraNum
+    for s=1:cameraNum
+        Y(r,s) = optx(r*cameraNum+s);
+    end
+end
+
+Xbinary = ceil(X'-0.5*rand(1,cameraNum));

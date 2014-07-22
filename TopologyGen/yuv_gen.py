33faf90f4f91646f6cabf2805282dbb66d8a3fa3
dennis5blue <dennis5blue@gmail.com>
2014-07-22 13:47:55 +0800

    Topology generation

--- TopologyGen/yuv_gen.py
+++ TopologyGen/yuv_gen.py
@@ -22,15 +22,17 @@ def main(argv=None):
  
 	processList = len(fileList)*[None] 
 	for i in range(len(fileList)):
-        	idx = int(re.sub(r'camera_|\.png','',fileList[i]))
-        	processList[idx] = re.sub(r'\.png','',fileList[i])
+		idx = int(re.sub(r'camera_|\.png','',fileList[i]))
+		processList[idx] = re.sub(r'\.png','',fileList[i])
+		#print (processList[idx])
+		#print (len(processList))
 
 	for i in range(len(fileList)):
         	print pngSeqPath+processList[i]+".png"
         	print yuvSeqPath+processList[i]+".yuv"
-        	call(["../ffmpeg","-i", pngSeqPath+processList[i]+".png","temp.mp4"])
-        	call(["../ffmpeg","-i", "temp.mp4",yuvSeqPath+processList[i]+".yuv"])
-        	call(["rm","temp.mp4"])
+        	#call(["../ffmpeg","-i", pngSeqPath+processList[i]+".png","temp.mp4"])
+        	#call(["../ffmpeg","-i", "temp.mp4",yuvSeqPath+processList[i]+".yuv"])
+        	#call(["rm","temp.mp4"])
 
 if __name__ == "__main__":
 	sys.exit(main())
